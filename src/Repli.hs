a929b2f31cdcf6556ec4c1c701b4818b44c79a73
Robin Palotai <palotai.robin@gmail.com>
2015-06-07 17:06:57 +0000

    Remove double-escaping in error messages

--- src/Repli.hs
+++ src/Repli.hs
@@ -11,6 +11,7 @@ import Control.Monad (join)
 import Data.Aeson (ToJSON(..), (.=), object)
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Monoid ((<>))
 import qualified Mueval.ArgsParse as Mu
 import qualified Mueval.Interpreter as Mu
 import qualified Language.Haskell.Interpreter as Hint
@@ -18,12 +19,17 @@ import System.Posix.Signals (sigXCPU, installHandler, Handler(CatchOnce))
 
 data Err
     = OptionError String
-    | InterpreterError Hint.InterpreterError
+    | InterpreterError Text
     | TimeoutError
     deriving (Show)
 
 instance ToJSON Err where
-    toJSON e = object ["error" .= T.pack (show e)]
+    toJSON e = object ["error" .= estr]
+      where
+        estr = case e of
+            OptionError s -> "Repli option error: " <> T.pack s
+            TimeoutError -> "Timeout"
+            InterpreterError t -> t
 
 data Result = Result
     { origExpr :: !Text
@@ -68,13 +74,20 @@ runSingle expr =
     go opt = do
         let i = Mu.interpreter opt
         fmap join $ Async.race
-            (d "timeout" timeout)
-            (d "runI" $ (fmap wrapRes $ Hint.runInterpreter i) >>= evaluate)
+            timeout
+            ((fmap wrapRes $ Hint.runInterpreter i) >>= evaluate)
       where
-        d _ = id
         t = T.pack
         timeout = threadDelay (10*1000000) >> return TimeoutError
-        wrapRes (Left err) = Left . InterpreterError $ err
+        wrapRes (Left err) =
+            let etext = case err of
+                    Hint.UnknownError s -> T.pack s
+                    Hint.NotAllowed s -> T.pack s
+                    Hint.GhcException s -> T.pack s
+                    Hint.WontCompile es -> T.intercalate "\n---\n"
+                                         . map (T.pack . Hint.errMsg)
+                                         $ es
+            in Left . InterpreterError $ etext
         wrapRes (Right (a,b,c)) =
             let res = t c
             in (T.length res) `seq` Right $! Result (t a) (t b) res
